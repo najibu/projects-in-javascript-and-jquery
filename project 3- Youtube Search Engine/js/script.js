9c803bd3e9310d9ba3a409943f6ad9cfac56c47b
najibu <1o1jibby@gmail.com>
2015-09-13 16:54:19 +0100

    Displaying results from API

--- project 3- Youtube Search Engine/js/script.js
+++ project 3- Youtube Search Engine/js/script.js
@@ -59,6 +59,53 @@ function search() {
 					// Display Results
 					$('#results').append(output);
 				});
+
+				var buttons = getButtons(prevPageToken, nextPageToken);
+
+				// Display Buttons
+				$('#buttons').append(buttons);
 			}
 	);
+}
+
+// Build Output
+function getOutput(item){
+	var videoId = item.id.videoId;
+	var title = item.snippet.title;
+	var description = item.snippet.description;
+	var thumb = item.snippet.thumbnails.high.url;
+	var channelTitle = item.snippet.channelTitle;
+	var videoDate = item.snippet.publishedAt;
+	
+	// Build Output String
+	var output = '<li>' +
+	'<div class="list-left">' +
+	'<img src="'+thumb+'">' +
+	'</div>' +
+	'<div class="list-right">' +
+	'<h3>'+title+'</h3>' +
+	'<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>' +
+	'<p>'+description+'</p>' +
+	'</div>' +
+	'</li>' +
+	'<div class="clearfix"></div>' +
+	'';
+	
+	return output;
+}
+
+function getButtons (prevPageToken, nextPageToken) {
+	if (!prevPageToken) {
+		var btnoutput = '<div class="button-container">'+'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' +
+		'onclick="nextPage();">Next Page</button></div>';
+
+	} else {
+		var btnoutput = '<div class="button-container">'+
+		'<button id="next-button" class="paging-button" data-token="'+prevPageToken+'" data-query="'+q+'"' +
+		'onclick="prevPage();">Prev Page</button>'+
+		'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' +
+		'onclick="nextPage();">Next Page</button></div>';
+	}
+
+	return btnoutput;
 }
