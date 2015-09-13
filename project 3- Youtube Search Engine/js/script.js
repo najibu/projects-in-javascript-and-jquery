943fede577b82d4031098e608ba731c1cf5bb829
najibu <1o1jibby@gmail.com>
2015-09-13 17:35:59 +0100

    Paging Buttons and lightbox

--- project 3- Youtube Search Engine/js/script.js
+++ project 3- Youtube Search Engine/js/script.js
@@ -68,6 +68,92 @@ function search() {
 	);
 }
 
+// Next Page function
+function nextPage() {
+	var token = $('#next-button').data('token');
+	var q = $('#next-button').data('token')
+
+	// Clear Results
+	$('#results').html('');
+	$('#buttons').html('');
+
+	// Get form input
+	q = $('#query').val();
+
+	// Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search", {
+			part: 'snippet, id',
+			q: q,
+			pageToken: token,
+			type: 'video',
+			key: 'AIzaSyBNMgpQzjjA75vUgsb0S5VtnykZblXSkc8'},
+			function(data) {
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+
+				// Log data
+				console.log(data);
+
+				$.each(data.items, function(i, item){
+					// Get Output
+					var output = getOutput(item);
+
+					// Display Results
+					$('#results').append(output);
+				});
+
+				var buttons = getButtons(prevPageToken, nextPageToken);
+
+				// Display Buttons
+				$('#buttons').append(buttons);
+			}
+	);	
+}
+
+// Prev Page function
+function prevPage() {
+	var token = $('#prev-button').data('token');
+	var q = $('#prev-button').data('token')
+
+	// Clear Results
+	$('#results').html('');
+	$('#buttons').html('');
+
+	// Get form input
+	q = $('#query').val();
+
+	// Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search", {
+			part: 'snippet, id',
+			q: q,
+			pageToken: token,
+			type: 'video',
+			key: 'AIzaSyBNMgpQzjjA75vUgsb0S5VtnykZblXSkc8'},
+			function(data) {
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+
+				// Log data
+				console.log(data);
+
+				$.each(data.items, function(i, item){
+					// Get Output
+					var output = getOutput(item);
+
+					// Display Results
+					$('#results').append(output);
+				});
+
+				var buttons = getButtons(prevPageToken, nextPageToken);
+
+				// Display Buttons
+				$('#buttons').append(buttons);
+			}
+	);	
+}
+
 // Build Output
 function getOutput(item){
 	var videoId = item.id.videoId;
@@ -83,7 +169,7 @@ function getOutput(item){
 	'<img src="'+thumb+'">' +
 	'</div>' +
 	'<div class="list-right">' +
-	'<h3>'+title+'</h3>' +
+	'<h3><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/'+videoId+'">'+title+'</a></h3>' +
 	'<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>' +
 	'<p>'+description+'</p>' +
 	'</div>' +
@@ -101,7 +187,7 @@ function getButtons (prevPageToken, nextPageToken) {
 
 	} else {
 		var btnoutput = '<div class="button-container">'+
-		'<button id="next-button" class="paging-button" data-token="'+prevPageToken+'" data-query="'+q+'"' +
+		'<button id="prev-button" class="paging-button" data-token="'+prevPageToken+'" data-query="'+q+'"' +
 		'onclick="prevPage();">Prev Page</button>'+
 		'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' +
 		'onclick="nextPage();">Next Page</button></div>';
